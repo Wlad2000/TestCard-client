75aa5b6f11d4c0eb1c2777150ed7303708f3bb61
Vladislav <wladgarshko@gmail.com>
2023-09-22 00:32:37 +0300

    addedChatAutoAswers

--- src/jsx/CardChat.jsx
+++ src/jsx/CardChat.jsx
@@ -1,65 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 import { Rectangle } from './QuickBase'
-import { MessageList } from "react-chat-elements";
+import { MessageBox, Input, Button } from "react-chat-elements";
 import "react-chat-elements/dist/main.css";
 import { useItemState } from './context';
 import '../scss/card.css'
-import { ReactChat } from "reactchatt";
-
+import { socket } from './App';
 
 
 const CardChat = () => {
     const { currentUser, imageData, reload, setReload, } = useItemState();
-    function child() {
-        return (
-        <div
-            style={{
-            width: "20px",
-            height: "20px",
-            border: "1px solid black",
-            "background-color": "red",
-            position: "relative",
-            display: "flex"
-        }}
-        ></div>
-           //<img style={{ border:'1px solid black', display: 'block', height:"7%", width: '7%'}} src={imageData} alt="Зображення" />
-        );
-      }
+
+    const [messages, setMessages] = useState([]);
+    const [messageText, setMessageText] = useState('');
      
-    
+    useEffect(() => {
+        socket.on('clientMessage', (message) => {
+          setMessages((prevMessages) => [...prevMessages, message]);
+        });
+      }, []);
+
+
+  const handleSendMessage = () => {
+    const message = { text: messageText, user: `${currentUser.login}` };
+
+    socket.emit('clientMessage', message);
+    setMessages((prevMessages) => [...prevMessages, message]);
+    setMessageText('');
+  };
+
   return (
     <Card width='400px' title="Card Chat">
-        <Rectangle>
-        <MessageList
-       className = 'ml'
-        lockable={true}
-        toBottomHeight={"100%"}
-        dataSource={[
-          {
-            position: "left",
-            type: "text",
-            title: "Name1",
-            titleColor: "red",
-            className : 'msg',
-            text:
-              "This is a long message that should have a long element near it. This is a long message that should have a long element near it",
-            date: new Date(),
-          },
-          {
-            position: "right",
-            type: "text",
-            title: "Name2",
-            text: "That's short",
-            date: new Date(),
-          }
-        ]}
-        children={[child()]}
-        isShowChild={true}
+
+      <Rectangle>
+        {messages.map((message, index) => (
+          <MessageBox
+            key={index}
+            title={message.user === 'auto' ? 'ChatAnswer' : `${currentUser.login}`}
+            position={message.user === 'auto' ? 'left' : 'right'}
+            type="text"
+            text={message.text}
+            date={new Date()}
+          />
+        ))}
+      </Rectangle>
+      <Input
+        placeholder="enter your question.."
+        multiline={true}
+        onChange={(e) => setMessageText(e.target.value)}
+        value={messageText}
+        rightButtons={
+          <Button
+            color="white"
+            backgroundColor="purple"
+            text="Send"
+            onClick={handleSendMessage}
+          />
+        }
       />
-        </Rectangle>
-        <Rectangle>
-        </Rectangle>
     </Card>
   )
 }
